c7b37db31ee154f1f2dccd9d068910236453eb4f
Scott Mitchell <scott@openobject.org>
2025-09-15 13:02:55 +1000

    added new example

--- examples/p5js example/sketch.js
+++ examples/p5js example/sketch.js
@@ -1,53 +1,81 @@
 // ==============================================================
 // P5js to Arduino Control
-// Basic example showing how to use JS2Arduino - arduinoComs.js
+// Basic example showing how to use P5js with JS2Arduino - arduinoComs.js
 // by Scott Mitchell
 // GPL-3.0 License
 // ==============================================================
 
-let ArduinoIP = 'ws://10.1.1.134:81/';
+let ArduinoIP = 'ws://172.20.10.13:81/';
 
 let arduino;
+let circleX, circleY;
+let dx, dy;
 
 // pin globals
-const LEDR = 4;
-const LEDG = 5;
-const LEDB = 6;
+const LED = 13;
 const POTPIN = A0;
 
 function setup() {
     createCanvas(600, 600);
 
+    // setup random variables
+    circleX = random(100, 500);
+    circleY = random(100, 500);
+    dx = random(-5, 5);
+    dy = random(-5, 5);
+
     // connect to Arduino
     arduino = new Arduino();
     arduino.connect(ArduinoIP);
 
-    // configure pins
-    arduino.pinMode(LEDR, OUTPUT);
-    arduino.pinMode(LEDG, OUTPUT);
-    arduino.pinMode(LEDB, OUTPUT);
-    // arduino.pinMode(POTPIN, ANALOG_INPUT, 200);
+    // configure pins for RGB LED
+    arduino.pinMode(LED, OUTPUT);
+    arduino.pinMode(POTPIN, ANALOG_INPUT, 100); // this is optional, the communication will work without it.
 }
 
 function draw() {
-    background(0);
+    background(50);
 
-    let dial = arduino.analogRead(SENSOR);
-    let brightness = map(dial, 0, 4095, 0, 255);
-    arduino.analogWrite(LED1, brightness);
-    let x = map(dial,0,4095,0,width);
-    circle(x,100,10);
-}
+    // get reading from the Arduino
+    let dial = arduino.analogRead(POTPIN);
+    let circleRadius = map(dial, 0, 1023, 2, 100);
 
-function mousePressed() {
-    // Fill all LEDs with blue
-    let c = arduino.neoPixel.Color(255, 0, 0);
-    arduino.neoPixel.fill(c, 0, 8);
-    arduino.neoPixel.show();
-}
+    // Check if mouse is inside the circle
+    let d = dist(mouseX, mouseY, circleX, circleY);
 
-function mouseReleased() {
-    arduino.neoPixel.clear();
-    arduino.neoPixel.setPixelColor(0, 255, 0, 0);
-    arduino.neoPixel.show();
+    if (d < circleRadius) {
+        // Mouse is inside the circle
+        fill(0, 250, 0); // Change color to green
+        // turn on LED on
+        arduino.digitalWrite(LED, HIGH);
+    } else {
+        // Mouse is outside the circle
+        fill(250, 250, 250); // Change color to white
+        // turn on LED off
+        arduino.digitalWrite(LED, LOW);
+    }
+
+    // draw circle on canvas
+    noStroke();
+    circle(circleX, circleY, circleRadius*2);
+
+    // move circle
+    circleX += dx;
+    circleY += dy;
+
+    // bounce circle of edge of canvas
+    if (circleX > width - circleRadius) {
+        circleX = width - circleRadius;
+        dx *= -1;
+    } else if (circleX < circleRadius) {
+        circleX = circleRadius;
+        dx *= -1;
+    }
+    if (circleY > height - circleRadius) {
+        circleY = height - circleRadius;
+        dy *= -1;
+    } else if (circleY < circleRadius) {
+        circleY = circleRadius;
+        dy *= -1;
+    }
 }
